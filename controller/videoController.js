48cec5b6a20535563f6f5d4de4ea458f5ed72881
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-12 00:53:37 +0900

    add many functions in controller modules

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,7 @@
 export const home = (req, res) => res.send("home");
 export const search = (req, res) => res.send("Video Search");
+
+export const videos = (req, res) => res.send("Videos");
 export const uploadVideo = (req, res) => res.send("Upload Video");
 export const editVideo = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
 import routes from "../routes";
+import {
+  uploadVideo,
+  editVideo,
+  deleteVideo,
+  videoDetail,
+  videos
+} from "../controller/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.home, (req, res) => res.send("Video"));
-videoRouter.get(routes.upload, (req, res) => res.send("Uplad Video"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routes.home, videos);
+videoRouter.get(routes.upload, uploadVideo);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
