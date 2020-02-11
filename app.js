cdbb2121f95593497dc662c143592427fd3128ba
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-11 23:47:44 +0900

    file name of "index.js" is changed to "app.js"

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ import morgan from "morgan";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-};
-
 const handleHome = (req, res) => res.send("Wetube");
 const handleProfile = (req, res) => res.send("Profile");
 
 app.use(cookieParser());
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
@@ -24,4 +18,4 @@ app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
