6e493d7c9b3f25337824d7077f403021dcb0db1e
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-12 00:34:03 +0900

    fix bugs about urls "/users/:id, /videos/:id"

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,8 @@ import routes from "../routes";
 const userRouter = express.Router();
 
 userRouter.get(routes.home, (req, res) => res.send("User Index"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
 userRouter.get(routes.editProfile, (req, res) => res.send("User Edit"));
 userRouter.get(routes.changePassword, (req, res) => res.send("User Password"));
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,8 +4,8 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.home, (req, res) => res.send("Video"));
 videoRouter.get(routes.upload, (req, res) => res.send("Uplad Video"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
 videoRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
 videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
 
 export default videoRouter;
