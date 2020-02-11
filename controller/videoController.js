e7cff8aebfaa065b11632e220b5503145779759b
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-12 00:47:40 +0900

    apply 'controllers' in global url

---
+++ controller/videoController.js
@@ -0,0 +1,6 @@
+export const home = (req, res) => res.send("home");
+export const search = (req, res) => res.send("Video Search");
+export const uploadVideo = (req, res) => res.send("Upload Video");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+export const videoDetail = (req, res) => res.send("Video Detail");
