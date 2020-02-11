64dbbdec4bae43c0d623bea8b3da0cf3184f05eb
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-12 00:11:53 +0900

    some changes of url name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
 app.use(cookieParser());
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 
