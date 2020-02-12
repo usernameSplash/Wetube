112e2d510290fb258c00211e076e2b23c114173c
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-13 00:39:56 +0900

    add logic in controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,11 +1,21 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  // const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+};
+
 export const uploadVideo = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
