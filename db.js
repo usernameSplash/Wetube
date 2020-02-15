d653d6adc0fffd975b11eda968a46d79c138c524
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-15 19:00:48 +0900

    add videos in home

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 123,
+    title: "video1",
+    description: "this is video1",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 1,
+      name: "creator1",
+      email: "cre@tor1.com"
+    }
+  },
+  {
+    id: 124,
+    title: "video2",
+    description: "this is video2",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2,
+      name: "creator2",
+      email: "cre@tor2.com"
+    }
+  },
+  {
+    id: 125,
+    title: "video3",
+    description: "this is video3",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 3,
+      name: "creator3",
+      email: "cre@tor3.com"
+    }
+  },
+  {
+    id: 126,
+    title: "video4",
+    description: "this is video4",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 4,
+      name: "creator4",
+      email: "cre@tor4.com"
+    }
+  }
+];
