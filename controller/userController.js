73f4f4934ead3d37054160113b178d383f934d9a
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-15 19:52:01 +0900

    add post method in join controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,21 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.render("join", { pageTitle: "Join" });
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do : Register User
+    // To Do : Log in User
+    res.redirect(routes.home);
+  }
 };
 
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
