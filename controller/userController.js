54e1798a517ab57d551dee35cc69b0b2a165f89b
Kyeongmin Kim <kkfenix3085@naver.com>
2020-02-15 19:41:58 +0900

    join controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,12 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.render("join", { pageTitle: "Join" });
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
--- controller/videoController.js
+++ controller/videoController.js
@@ -9,7 +9,11 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+  res.render("search", {
+    pageTitle: "Search",
+    searchingBy: searchingBy,
+    videos
+  });
 };
 
 export const uploadVideo = (req, res) =>
