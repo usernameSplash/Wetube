67bd66ca5b364694a265a77cb3051c824793b056
usernameSplash <kkfenix3085@naver.com>
2020-02-20 23:07:03 +0900

    add 'a' tag to video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,13 @@
 import express from "express";
 import routes from "../routes";
 import {
-  uploadVideo,
-  editVideo,
-  deleteVideo,
-  videoDetail,
-  videos
+    uploadVideo,
+    editVideo,
+    deleteVideo,
+    videoDetail,
 } from "../controller/videoController";
 const videoRouter = express.Router();
 
-// videoRouter.get(routes.home, videos);
 videoRouter.get(routes.upload, uploadVideo);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
