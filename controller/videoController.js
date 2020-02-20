a356858c357c9e96a5c01a6c65e1502d7e0641dd
usernameSplash <kkfenix3085@naver.com>
2020-02-20 23:47:20 +0900

    add 'post' in '/upload'

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,29 +1,35 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+    res.render("home", { pageTitle: "Home", videos });
 };
 
 export const search = (req, res) => {
-  // const searchingBy = req.query.term;
-  const {
-    query: { term: searchingBy }
-  } = req;
-  res.render("search", {
-    pageTitle: "Search",
-    searchingBy: searchingBy,
-    videos
-  });
+    // const searchingBy = req.query.term;
+    const {
+        query: { term: searchingBy },
+    } = req;
+    res.render("search", {
+        pageTitle: "Search",
+        searchingBy: searchingBy,
+        videos,
+    });
 };
 
-export const uploadVideo = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
-
+export const getUpload = (req, res) =>
+    res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description },
+    } = req;
+    res.redirect(routes.videoDetail(123));
+};
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+    res.render("videoDetail", { pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+    res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+    res.render("deleteVideo", { pageTitle: "Delete Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "../routes";
 import {
-    uploadVideo,
     editVideo,
     deleteVideo,
     videoDetail,
+    getUpload,
+    postUpload,
 } from "../controller/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, uploadVideo);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.videoDetail(), videoDetail);
