638e9f132f0af12c5dc0bf7c82cf966a5a5d5ff0
usernameSplash <kkfenix3085@naver.com>
2020-02-23 18:06:44 +0900

    handle "getEdit"

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,19 +1,27 @@
 import express from "express";
 import routes from "../routes";
 import {
-    editVideo,
     deleteVideo,
     videoDetail,
     getUpload,
     postUpload,
+    getEditVideo,
+    postEditVideo,
 } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// DeleteVideo
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
