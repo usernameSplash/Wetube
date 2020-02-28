395b974c0bf66bfa1c740ce07bcb404fecdd1f1a
usernameSplash <kkfenix3085@naver.com>
2020-02-29 00:04:48 +0900

    handle login

--- middlewares.js
+++ middlewares.js
@@ -7,6 +7,6 @@ export const uploadVideo = multerVideo.single("videoFile");
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
-    res.locals.user = req.user || {};
+    res.locals.user = req.user || null;
     next();
 };
